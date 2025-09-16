afff4c344b7305bbaa164f20acb1a39834008aef
Connor Hudson <connorqhudson2020@gmail.com>
2025-09-15 21:38:23 -0500

    create "setup.sh"

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+### Superstar Config ###
+#
+# This setup script will move related conf files to their 
+# designated directories, and then load them for usage.
+#
+########################
+
+# .bash_aliases is a special bash conf for aliases and is loaded in .bashrc
+# It's a good idea to store aliases here instead of editing .bashrc too much.
+cp --force bash/bash_aliases ~/.bash_aliases 
