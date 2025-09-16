7b3e597d9bddf46db2a67d614e80fb6fa84bbb28
Connor Hudson <connorqhudson2020@gmail.com>
2025-09-15 21:43:16 -0500

    load .bash_aliases with "source"

--- setup.sh
+++ setup.sh
@@ -10,3 +10,5 @@
 # .bash_aliases is a special bash conf for aliases and is loaded in .bashrc
 # It's a good idea to store aliases here instead of editing .bashrc too much.
 cp --force bash/bash_aliases ~/.bash_aliases 
+source ~/.bash_aliases
+
